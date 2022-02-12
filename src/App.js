d52b34687409fd9543f6a5f3489f30699fba6815
ASUS <Yehor Kholodiuk>
2022-02-12 20:53:16 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,6 +15,16 @@ const candyColors = [
 const App = () => {
     const [currentColorArrangement,setCurrentColorArrangement] = useState([])
 
+    const checkForColumnOfThree = () => {
+        for (let i = 0; i < 47; i++){
+            const columnOfThree = [i, i + width, i + width*2]
+            const decidedColor = currentColorArrangement[i]
+            if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor)){
+                columnOfThree.forEach(square => currentColorArrangement[square] = '')
+            }
+        }
+    }
+
   const createBoard = () =>{
     const randomColorArrangement = [];
 for (let i = 0; i < width*width;i++) {
@@ -28,6 +38,18 @@ for (let i = 0; i < width*width;i++) {
 useEffect(() => {
     createBoard()
 },[])
+
+
+    useEffect ( () => {
+        const timer = setInterval( () => {
+            checkForColumnOfThree()
+            setCurrentColorArrangement([...currentColorArrangement])
+            },100)
+
+        return () => clearInterval(timer)
+        }, [checkForColumnOfThree, currentColorArrangement]
+
+    )
     console.log(currentColorArrangement)
 
     return (
