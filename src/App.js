574778bf30e3435348f2ae2e2780a3d294a4293e
ASUS <Yehor Kholodiuk>
2022-02-13 14:01:07 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,15 +15,41 @@ const candyColors = [
 const App = () => {
     const [currentColorArrangement,setCurrentColorArrangement] = useState([])
 
+
+    const checkForColumnOfFour = () => {
+        for (let i = 0; i < 39; i++){
+            const columnOfFour = [i, i + width, i + width * 2, i + width * 3]
+            const decidedColor = currentColorArrangement[i]
+            if (columnOfFour.every(square => currentColorArrangement[square] === decidedColor)){
+                columnOfFour.forEach(square => currentColorArrangement[square] = '')
+            }
+        }
+    }
+
     const checkForColumnOfThree = () => {
         for (let i = 0; i < 47; i++){
-            const columnOfThree = [i, i + width, i + width*2]
+            const columnOfThree = [i, i + width, i + width * 2]
             const decidedColor = currentColorArrangement[i]
             if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor)){
                 columnOfThree.forEach(square => currentColorArrangement[square] = '')
             }
         }
     }
+
+    const checkForRowOfThree = () => {
+        for (let i = 0; i < 64; i++){
+            const RowOfThree = [i, i + 1, i + 2]
+            const decidedColor = currentColorArrangement[i]
+            const notValid = [6,7,14,15,22,23,30,31,38,39,46,47,54,55,63,64]
+            if (notValid.includes(i)) continue
+
+           if (RowOfThree.every(square => currentColorArrangement[square] === decidedColor)){
+                RowOfThree.forEach(square => currentColorArrangement[square] = '')
+            }
+        }
+    }
+
+
 
   const createBoard = () =>{
     const randomColorArrangement = [];
@@ -42,12 +68,15 @@ useEffect(() => {
 
     useEffect ( () => {
         const timer = setInterval( () => {
+
+            checkForColumnOfFour()
             checkForColumnOfThree()
+            checkForRowOfThree()
             setCurrentColorArrangement([...currentColorArrangement])
             },100)
 
         return () => clearInterval(timer)
-        }, [checkForColumnOfThree, currentColorArrangement]
+        }, [checkForColumnOfFour,checkForColumnOfThree,checkForRowOfThree, currentColorArrangement]
 
     )
     console.log(currentColorArrangement)
@@ -72,3 +101,5 @@ useEffect(() => {
 };
 
 export default App;
+
+
